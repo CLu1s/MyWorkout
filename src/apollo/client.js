15d6a9797e056da0d10da3a703571f406ec7d5db
Luis L <luis.luis@icloud.com>
2019-04-10 22:49:47 -0500

    frontend(graphql): fix uri

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -2,6 +2,6 @@ import ApolloClient from 'apollo-boost';
 import fetch from 'isomorphic-fetch';
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://my-workout-be.herokuapp.com/',
   fetch
 })
